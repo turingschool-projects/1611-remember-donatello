f1fadc3e0d8aa0e2acbcb37185e15060d528bc64
Charles Dunn <dunnc1551@gmail.com>
2017-04-05 18:05:53 -0600

    Remove unneeded actions

--- app/controllers/reminders.js
+++ app/controllers/reminders.js
@@ -1,12 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  actions: {
-    addReminder: function() {
-      let reminderTitle = this.get('reminderTitle');
-      let reminderBody = this.get('reminderBody');
-      let reminderDate = this.get('reminderDate');
-    },
-  }
-    
+
 });
