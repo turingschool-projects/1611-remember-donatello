5b8f5e82e7046ab92480dfb7ceb6436992d79776
Charles Dunn <dunnc1551@gmail.com>
2017-04-05 18:07:03 -0600

    Update add reminder action

--- app/controllers/reminders/new.js
+++ app/controllers/reminders/new.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    addReminder(model) {
+    addReminder: function(model) {
       model.save().then(() => {
         this.transitionToRoute('reminders');
       })
-    }
+    },
   }
 });
